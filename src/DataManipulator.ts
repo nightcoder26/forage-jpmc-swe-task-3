12499463984cd333d5a921b570ab807c2b56d79c
Knight <77844703+nightcoder26@users.noreply.github.com>
2024-04-24 00:49:24 +0530

    Updated Graph.tsx to display ratio instead of top bid or ask prices && Updated DataManipulate.ts to facilitate the former

--- src/DataManipulator.ts
+++ src/DataManipulator.ts
@@ -1,20 +1,37 @@
-import { ServerRespond } from './DataStreamer';
+import { ServerRespond } from "./DataStreamer";
 
 export interface Row {
-  stock: string,
-  top_ask_price: number,
-  timestamp: Date,
+  price_abc: number;
+  price_def: number;
+  ratio: number;
+  timestamp: Date;
+  lower_bound: number;
+  upper_bound: number;
+  trigger: number;
 }
-
 
 export class DataManipulator {
   static generateRow(serverResponds: ServerRespond[]) {
+    const priceABC =
+      (serverResponds[0].top_ask.price + serverResponds[0].top_bid.price) / 2;
+    const priceDEF =
+      (serverResponds[1].top_ask.price + serverResponds[1].top_bid.price) / 2;
+    const ratio = priceABC / priceDEF;
+    const upperBound = 1 + 0.5;
+    const lowerBound = 1 - 0.5;
     return serverResponds.map((el: any) => {
       return {
-        stock: el.stock,
-        top_ask_price: el.top_ask && el.top_ask.price || 0,
-        timestamp: el.timestamp,
+        price_abc: priceABC,
+        price_def: priceDEF,
+        ratio: ratio,
+        timeStamp:
+          serverResponds[0].timestamp > serverResponds[1].timestamp
+            ? serverResponds[0].timestamp
+            : serverResponds[1].timestamp,
+        upper_bound: upperBound,
+        lower_bound: lowerBound,
+        trigger: ratio > upperBound || ratio < upperBound ? ratio : undefined,
       };
-    })
+    });
   }
 }

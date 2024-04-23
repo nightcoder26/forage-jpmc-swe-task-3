14028668e520a9ae35a5e75bf021df8d59dccdbc
Knight <77844703+nightcoder26@users.noreply.github.com>
2024-04-24 01:14:05 +0530

    minor fixes

--- src/DataManipulator.ts
+++ src/DataManipulator.ts
@@ -11,28 +11,47 @@ export interface Row {
 }
 
 export class DataManipulator {
-  static generateRow(serverResponds: ServerRespond[]) {
+  static generateRow(serverRespond: ServerRespond[]) {
     const priceABC =
-      (serverResponds[0].top_ask.price + serverResponds[0].top_bid.price) / 2;
+      (serverRespond[0].top_ask.price + serverRespond[0].top_bid.price) / 2;
     const priceDEF =
-      (serverResponds[1].top_ask.price + serverResponds[1].top_bid.price) / 2;
+      (serverRespond[1].top_ask.price + serverRespond[1].top_bid.price) / 2;
     const ratio = priceABC / priceDEF;
     const upperBound = 1 + 0.5;
     const lowerBound = 1 - 0.5;
-    return serverResponds.map((el: any) => {
-      return {
-        price_abc: priceABC,
-        price_def: priceDEF,
-        ratio: ratio,
-        timeStamp:
-          serverResponds[0].timestamp > serverResponds[1].timestamp
-            ? serverResponds[0].timestamp
-            : serverResponds[1].timestamp,
-        upper_bound: upperBound,
-        lower_bound: lowerBound,
-        trigger_alert:
-          ratio > upperBound || ratio < upperBound ? ratio : undefined,
-      };
-    });
+
+    return {
+      price_abc: priceABC,
+      price_def: priceDEF,
+      ratio: ratio,
+      timeStamp:
+        serverRespond[0].timestamp > serverRespond[1].timestamp
+          ? serverRespond[0].timestamp
+          : serverRespond[1].timestamp,
+      upper_bound: upperBound,
+      lower_bound: lowerBound,
+      trigger_alert:
+        ratio > upperBound || ratio < upperBound ? ratio : undefined,
+    };
   }
 }
+
+// export class DataManipulator {
+//   static generateRow(serverRespond: ServerRespond[]): Row {
+//       const priceABC = (serverRespond[0].top_ask.price + serverRespond[0].top_bid.price) / 2;
+//       const priceDEF = (serverRespond[1].top_ask.price + serverRespond[1].top_bid.price) / 2;
+//       const ratio = priceABC / priceDEF;
+//       const upperBound = 1 + 0.05;
+//       const lowerBound = 1 - 0.05;
+//       return {
+//           price_abc: priceABC,
+//           price_def: priceDEF,
+//           ratio,
+//           timestamp: serverRespond[0].timestamp > serverRespond[1].timestamp ?
+//               serverRespond[0].timestamp : serverRespond[1].timestamp,
+//           upper_bound: upperBound,
+//           lower_bound: lowerBound,
+//           trigger_alert: (ratio > upperBound || ratio < lowerBound) ? ratio : undefined,
+//       };
+//   }
+// }
